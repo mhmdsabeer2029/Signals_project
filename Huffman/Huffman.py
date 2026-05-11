0bd4859c9ed52849f62636e3b91d6efb52acb7ad
EnAbdulrahman <es.abdelrahman.sief2024@alexu.edu.eg>
2026-05-11 12:05:54 +0300

    Added Node class and made frequency counter with lists as per the report

--- Huffman/Huffman.py
+++ Huffman/Huffman.py
@@ -1,12 +1,18 @@
 import heapq
+class Node:
+    def __init__(self , symbol ,freq):
+        self.symbol = symbol
+        self.freq = freq
+        self.left = None
+        self.right = None
 
-def symbol_counter(symbols):
-    literal_dict = {}
-    distance_dict = {}
-    for  symbol in symbols:
-        if isinstance(symbol, int):
-            literal_dict[symbol] = literal_dict.get(symbol, 0) + 1
-        elif isinstance(symbol, tuple):
-            literal_dict[symbol[0]] = literal_dict.get(symbol[0] , 0) + 1
-            distance_dict[symbol[2]] = distance_dict.get(symbol[2] , 0) + 1
-    return literal_dict, distance_dict
+def frequency_counter(symbols):
+    literal_freq = [0] * 286
+    distance_freq = [0] * 30
+    for sym in symbols:
+        if isinstance(sym, int):
+            literal_freq[sym] += 1
+        elif isinstance(sym, tuple):
+            literal_freq[sym[0]] += 1
+            distance_freq[sym[2]] += 1
+    return literal_freq, distance_freq
